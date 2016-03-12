727d6cf41f9d892b965d4def24557e8d7351b2cc
wuSparky <wuyanping0711@163.com>
2016-03-12 22:00:14 +0800

    add empty crawl movie for jianglu

---
+++ crawl_qq_movie.py
@@ -0,0 +1,21 @@
+#!/bin/python
+# coding: utf-8
+
+import os
+import re
+import sys
+import urllib2
+
+###############################################################################
+##  SUMMARY : crawl movie from qq.com
+##  AUTHOR  : JiangLu
+##  TIME    :
+###############################################################################
+
+
+def download(index):
+    return
+
+def parse(html):
+    return
+
